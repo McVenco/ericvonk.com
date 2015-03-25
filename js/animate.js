5961b21f8c22ed0f4d028ac8dd4cef5d3c6232ea
McVenco <mcvenco@gmail.com>
2015-03-25 23:40:09 +0100

    - uncommented jQuery in index.html header + added jQueryRotate plugin link - added jQueryRotate code to js/animate.js

--- js/animate.js
+++ js/animate.js
@@ -1,1 +1,17 @@
 // empty file, to be filled
+$(document).ready(function(){
+
+	$("h1").rotate({ 
+	   bind: 
+	     { 
+	        mouseover : function() { 
+	            $(this).rotate({animateTo:360})
+	        },
+	        mouseout : function() { 
+	            $(this).rotate({animateTo:0})
+	        }
+	     } 
+	   
+	});
+
+});
