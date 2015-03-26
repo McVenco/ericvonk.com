c3f15af2e540e0b0732718acbc4dff2b5ac95ef0
McVenco <mcvenco@gmail.com>
2015-03-26 10:01:18 +0100

    change text inside circles to become bold on mouseover

--- js/animate.js
+++ js/animate.js
@@ -3,14 +3,21 @@ $(document).ready(function(){
 	$("h1").rotate({ 
 	   bind: 
 	     { 
-	        mouseover : function() { 
-	            $(this).rotate({animateTo:360})
-	        },
-	        mouseout : function() { 
-	            $(this).rotate({animateTo:0})
-	        }
+        mouseover : function() { 
+            $(this).rotate({animateTo:360})
+        },
+        mouseout : function() { 
+            $(this).rotate({animateTo:0})
+        }
 	     } 
-	   
 	});
+
+	$("h1").hover(
+		function(){
+			$(this).css('font-weight', 'bold');
+		}, function(){
+			$(this).css('font-weight', 'normal');
+		}
+	);
 
 });
