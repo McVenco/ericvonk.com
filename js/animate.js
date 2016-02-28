4e3fb00a70432bd5cff4bb7d8c94d9b5158d8774
McVenco <mcvenco@gmail.com>
2016-02-28 20:49:40 +0100

    indents

--- js/animate.js
+++ js/animate.js
@@ -1,27 +1,27 @@
 $(document).ready(function(){
 
-	$(".circle.color-dark").rotate({ 
-	   bind: 
-	     { 
-        mouseover : function() { 
-            $(this).rotate({animateTo:360})
-        },
-        mouseout : function() { 
-            $(this).rotate({animateTo:0})
-        }
-	     } 
+	$(".circle.color-dark").rotate({
+		bind:
+		{
+			mouseover : function(){
+				$(this).rotate({animateTo:360})
+			},
+			mouseout : function(){
+				$(this).rotate({animateTo:0})
+			}
+		}
 	});
 
-	$(".circle.color-light").rotate({ 
-	   bind: 
-	     { 
-        mouseover : function() { 
-            $(this).rotate({animateTo:-360})
-        },
-        mouseout : function() { 
-            $(this).rotate({animateTo:0})
-        }
-	     } 
+	$(".circle.color-light").rotate({
+		bind:
+		{
+			mouseover : function(){
+				$(this).rotate({animateTo:-360})
+			},
+			mouseout : function({
+				$(this).rotate({animateTo:0})
+			}
+		}
 	});
 
 	$("h1").hover(
